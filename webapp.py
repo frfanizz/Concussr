0b946c38e3c41a80aa3f39d7ea43d1cbca2dfcda
Artemish <mitch.cro@gmail.com>
2018-01-28 04:12:36 -0600

    Moar refactoring

--- webapp.py
+++ webapp.py
@@ -4,12 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    t1 = test_render(1)
-    t2 = test_render(2)
-    return render_template("reading_test.html", test_1_lines=t1, test_2_lines=t2)
+    t1_lines, t1_solution = test_render(1)
+    t2_lines, t2_solution = test_render(2)
+    return render_template("reading_test.html",
+            t1_lines = t1_lines, t1_solution = t1_solution,
+            t2_lines = t2_lines, t2_solution = t2_solution)
 
 def test_render(ver):
-    return generateTest(testVersion=ver)[0].replace(' ', '\xa0').split('\n')
+    text, solution = generateTest(testVersion=ver)
+    return (text.replace(' ', '\xa0').split('\n'), solution)
 
 if __name__ == '__main__':
     app.run(debug=True)
