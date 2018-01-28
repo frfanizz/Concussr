1c3389c54eddd570ae6eb7bd1249b3ab583fe540
Artemish <mitch.cro@gmail.com>
2018-01-28 10:44:37 -0600

    centering

---
+++ transcribe.py
@@ -0,0 +1,45 @@
+"""Google Cloud Speech API sample application using the REST API for batch
+processing.
+Example usage:
+    python transcribe.py resources/audio.raw
+    python transcribe.py gs://cloud-samples-tests/speech/brooklyn.flac
+"""
+
+import argparse
+import io
+
+def transcribe_file(speech_file):
+    """Transcribe the given audio file."""
+    from google.cloud import speech
+    from google.cloud.speech import enums
+    from google.cloud.speech import types
+    client = speech.SpeechClient()
+
+    with io.open(speech_file, 'rb') as audio_file:
+        content = audio_file.read()
+
+    audio = types.RecognitionAudio(content=content)
+    config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=16000,
+        language_code='en-US')
+
+    response = client.recognize(config, audio)
+
+    # Each result is for a consecutive portion of the audio. Iterate through
+    # them to get the transcripts for the entire audio file.
+    for result in response.results:
+        # The first alternative is the most likely one for this portion.
+        print('Transcript: {}'.format(result.alternatives[0].transcript))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument(
+        'path', help='File or GCS path for audio file to be recognized')
+    args = parser.parse_args()
+    if args.path.startswith('gs://'):
+        transcribe_gcs(args.path)
+    else:
+        transcribe_file(args.path)
