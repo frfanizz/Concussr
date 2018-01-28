92ab476daba74630fe474b894c518031f5c074fe
ffanizza <ffanizza@hawk.iit.edu>
2018-01-28 01:44:16 -0600

    added elapsed timer timer

--- templates/record.js
+++ templates/record.js
@@ -1,22 +1,33 @@
 
+var AvailableNumbers = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
+$(document).ready(function (window) {
+    var isRecording = false;
+    // var isBusy = false;
+    var startTime, endTime, elapsedTime;
+    document.getElementById("recordBtn").onclick = function(oClick) {
+        // isBusy = true;
+        if (!isRecording) {
+            isRecording = true;
+            // Start recording speech
+            // TODO:
+            // Display test text
+            // TODO:
+            // Start timer
+            startTime = new Date();
+            // Change button to end timer
+            oClick.path[0].textContent = "End test";
+        } else {
+            isRecording = false;
+            // End timer
+            endTime = new Date();
+            // Disable Button
+            oClick.path[0].textContent = "Test Over";
+            oClick.path[0].disabled = true;
+            elapsedTime = (endTime - startTime)/1000;
+            document.getElementById("testResults").textContent = "Elapsed time = " + elapsedTime + " seconds";
+        }
+        
+    };
 
-// HTML audio element
-var player = document.getElementById('htmlAudioElement');
-
-// On handling successful receipt of user's audio recorder
-var handleSuccess = function(stream) {
-    // Show test text, begin timer, begin recording
-
-
-
-    
-    if (window.URL) {
-        player.src = window.URL.createObjectURL(stream);
-    } else {
-        player.src = stream;
-    }
-};
-
-// Tries to get user's audio recording device
-navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(handleSuccess);
+})
